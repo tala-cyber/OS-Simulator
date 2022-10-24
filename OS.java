0f58a8ce1fe545577dd80da06f0a3e45232f4024
tala-cyber <96355494+tala-cyber@users.noreply.github.com>
2022-10-24 01:26:00 -0400

    fixed fatal bug!

--- OS.java
+++ OS.java
@@ -24,7 +24,7 @@ public class OS {
     static ArrayList<Process> Ready = new ArrayList<Process>();
     static ArrayList<Process> IO = new ArrayList<Process>();
     static ArrayList<PCB> Memory=new ArrayList<PCB>();
-    static int stats;
+    static int stats; static int lock;
 
     static class Operation{
         public String type; public int cycles;
@@ -37,17 +37,18 @@ public class OS {
     }
 
     static class PCB{
-        public int PID; public ArrayList<Operation> ops; public String state; int curr_op;
+        public int PID; public ArrayList<Operation> ops; public String state; public static int curr_op;
         public PCB(Process p){PID=p.PID; ops=p.ops; state=p.state; curr_op=0;}
     }
 
-    public static PCB searchMemory(Process p){
+    public static int searchMemory(Process p){
         for(int i=0; i<Memory.size(); i++){
             if(Memory.get(i).PID==p.PID){
-                return Memory.get(i);
+                return i;
             }
         }
-        return new PCB(p);
+        Memory.add(new PCB(p));
+        return Memory.size()-1;
     }
 
     public static Process createProcess(String template) throws SAXException, IOException, ParserConfigurationException{
@@ -98,8 +99,10 @@ public class OS {
         }        
     }
 
-    public static synchronized void CPU(Operation op, int quantum) throws InterruptedException{
-        Object ob = new Object();
+    public static synchronized void CPU(Process p,int quantum) throws InterruptedException{
+        Acquire();
+        PCB block = Memory.get(searchMemory(p));
+        Operation op=block.ops.get(block.curr_op);
         // each cycle is 10 milliseconds and the time quantum is 2 cycles
         if(quantum==0 || op.cycles<quantum){
             for(int i=0; i<op.cycles; i++){
@@ -112,9 +115,19 @@ public class OS {
             }
             op.cycles=op.cycles-quantum;
         }
+        Release();
     }
 
-    public static Process Fork(Process p, Operation op) throws SAXException, IOException, ParserConfigurationException{
+    public static void Acquire(){
+        lock++;
+    }
+    public static void Release(){
+        lock--;
+    }
+
+    public static Process Fork(Process p) throws SAXException, IOException, ParserConfigurationException{
+        PCB block = Memory.get(searchMemory(p));
+        Operation op=block.ops.get(block.curr_op);
         op.cycles=0;
         if(p.template==1){
             return createProcess("pf1.xml");
@@ -147,78 +160,39 @@ public class OS {
         while(!Processes.isEmpty()){
             if(!Ready.isEmpty()){
                 p=Ready.get(0);
-                PCB block = searchMemory(p);
+                PCB block = Memory.get(searchMemory(p));
                 op=block.ops.get(block.curr_op);
-                CPU(op, 2);
-                ClassifyProcess(p, block);
+                CPU(p, 2);
+                ClassifyProcess(p);
                 updateIO();
                 printStatsRR();
+            }else if(IO.isEmpty()){
+                break;
             }else{
                 updateIO();
                 printStatsRR();
             }
-
-
         }
-        /*
-        while(!Processes.isEmpty()){
-            p=Processes.get(0);
-            PCB block = searchMemory(p);
-            op=block.ops.get(block.curr_op);
-            if(op.type.equals("calculate")){
-                block.state="RUNNING";
-                CPU(op,2);
-            }else if(op.type.equals("io")){
-                block.state="WAITING";
-                IO(op,2);
-            }else{
-                Processes.add(Fork(p));
-            }
-            if(op.cycles>=2){
-                op.cycles-=2;
-                if(op.type.equals("calculate")){
-                    block.state="READY";
-                }else{
-                    block.state="WAITING";
-                }
-            }else{
-                op.cycles=0;
-                if(block.curr_op==4){
-                    Processes.remove(p);
-                    block.state="TERMINATED";
-                }else{
-                    block.curr_op++;
-                    Processes.remove(p); Processes.add(p);
-                    if(block.ops.get(block.curr_op).type.equals("calculate")){
-                        block.state="READY";
-                    }else{
-                        block.state="WAITING";
-                    }   
-                }   
-            }
-
-        }
-        */
+        
     }
 
     public static void updateIO() throws InterruptedException, SAXException, IOException, ParserConfigurationException{
         for(int i=0; i<IO.size(); i++){
             Process p =IO.get(i);
-            PCB block = searchMemory(p);
+            PCB block = Memory.get(searchMemory(p));
             Operation op=block.ops.get(block.curr_op);
             op.cycles=op.cycles-2;
         }
-        int size=IO.size();
+        ArrayList<Process> copyIO = new ArrayList<>(IO);
+        int size=copyIO.size();
         for(int i=0; i<size; i++){
-            Process p =IO.get(i);
-            PCB block = searchMemory(p);
-            
-            ClassifyProcess(p, block);
+            Process p =copyIO.get(i);
+            ClassifyProcess(p);
         }
     }
 
-    public static void ClassifyProcess(Process p, PCB given) throws InterruptedException, SAXException, IOException, ParserConfigurationException{
-        PCB block = given;
+    public static void ClassifyProcess(Process p) throws InterruptedException, SAXException, IOException, ParserConfigurationException{
+        PCB block = Memory.get(searchMemory(p));
         Operation op=block.ops.get(block.curr_op);
         IO.remove(p); Ready.remove(p);
         if(op.cycles>0){
@@ -232,14 +206,15 @@ public class OS {
                 Processes.remove(p); 
             }else{
                 block.curr_op++;
+                System.out.println("increment"); //System.exit(1);
                 op=block.ops.get(block.curr_op);
                 if(op.type.equals("calculate")){
                     Ready.add(p);
                 }else if(op.type.equals("io")){
                     IO.add(p);
                 }else{
-                    Fork(p, op);
-                    ClassifyProcess(p, block);
+                    Fork(p);
+                    ClassifyProcess(p);
                 }
             }
         }
@@ -264,18 +239,20 @@ public class OS {
         }else{
             System.out.println("Ready: ");
             for(int i=0; i<Ready.size(); i++){
-                System.out.print(Ready.get(i).PID+"--");
+                System.out.print(Ready.get(i).PID);
+                System.out.print("("+Memory.get(searchMemory(Ready.get(i))).curr_op+")--");
             }
             System.out.println();
             System.out.println("Waiting:   ");
             for(int i=0; i<IO.size(); i++){
-                System.out.print(IO.get(i).PID+"--");
+                System.out.print(IO.get(i).PID);
+                System.out.print("("+Memory.get(searchMemory(IO.get(i))).curr_op+")--");
             }
             System.out.println();
             if(Ready.size()>=1){
-                System.out.println("Runnining:  "+Ready.get(0).PID);
+                System.out.println("Running:  "+Ready.get(0).PID);
             }else{
-                System.out.println("Runnining:  ");
+                System.out.println("Running:  ");
             }
             System.out.println(); System.out.println();
 
@@ -288,11 +265,11 @@ public class OS {
             for(int j=0; j<5; j++){
                 Operation op = p.ops.get(j);
                 if(op.type.equals("calculate")){
-                    CPU(op, 0);
+                    CPU(p, 0);
                 }else if(op.type.equals("io")){
                     IO(op, 0);
                 }else{
-                    Fork(p, op);
+                    Fork(p);
                 }
             }
             printStatsFCFS(i);
@@ -353,19 +330,10 @@ public class OS {
         String exit = myObj.nextLine();
         if(exit.equals("x")){
 
-        }
-
-        /*
-         * System.out.println("Type x to exit");
-            String exit = myObj.nextLine();
-            if(exit=="x"){
-
-            }
-         * 
-         */
-            
+        }          
         
 
     }
 }
+
 
